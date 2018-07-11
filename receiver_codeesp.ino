1ea4ed42e61ec90581a87d2e66156710edb7cdf0
ghufranm <mustaan.ghufran@gmail.com>
2018-07-11 20:25:23 +0700

    Add files via upload

---
+++ receiver_codeesp.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+
+SoftwareSerial NodeSerial(D3,D2);
+
+void setup(){
+	//pinMode(D2,INPUT);
+  //pinMode(D3,OUTPUT);
+	Serial.begin(115200);
+	//Serial.begin(9600);
+	NodeSerial.begin(4800);
+
+}
+
+void loop(){
+	//int sensorValue = analogRead(A0);
+	//NodeMCU.print(sensorValue);
+  //NodeSerial.print(10); 
+	//NodeSerial.println("\n");
+  while(NodeSerial.available()>0);
+  float val = NodeSerial.parseFloat();
+  if(NodeSerial.read()== '\n')
+  Serial.println(val);
+	delay(30);
+}
---
+++ sender_codeuno.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial ArduinoSerial(2,3);
+
+void setup(){
+	
+	//Serial.begin(9600);
+  Serial.begin(9600);
+	ArduinoSerial.begin(4800);
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+
+}
+
+void loop(){
+
+  int sensorValue = analogRead(A0); //nilai diganti dengan input LAT LONG
+	ArduinoSerial.print(sensorValue);
+  ArduinoSerial.println("\n");
+	//while(ArduinoSerial.available()>0){
+	//float val = ArduinoSerial.parseFloat();
+	//if(ArduinoSerial.read()== '\n'){
+	//Serial.println(val);
+	
+
+delay(30);
+}
